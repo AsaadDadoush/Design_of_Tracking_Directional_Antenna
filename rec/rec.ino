1cc6cf76b8bae1e3f6dde0cb961dd09ab9a09bfd
AsaadDadoush <asaaddaadouch246@gmai.com>
2022-10-24 14:06:58 +0300

    Serial-alt

--- rec/rec.ino
+++ rec/rec.ino
@@ -2,10 +2,12 @@ String a;
 String rssi_ser =" ";
 String latitude_ser = " ";
 String longitude_ser = " ";
+String altitude_ser = " ";
 
 int rssi=0;
 float latitude =0.0000000;
 float longitude=0.0000000;
+float altitude = 0.0000000;
 
 void setup() {
 
@@ -19,8 +21,9 @@ while(Serial.available()) {
 
 a= Serial.readString();// read the incoming data as string
 rssi_ser= a.substring(0,3); rssi = rssi_ser.toInt();
-latitude_ser= a.substring(4,12); latitude = latitude_ser.toFloat();
-longitude_ser= a.substring(14,22); longitude = longitude_ser.toFloat(); 
+latitude_ser= a.substring(6,15); latitude = latitude_ser.toFloat();
+longitude_ser= a.substring(18,26); longitude = longitude_ser.toFloat();
+altitude_ser = a.substring(28,32); altitude = altitude_ser.toFloat();
 //Serial.print("RSSI:      ");Serial.println(rssi_ser);
 //Serial.print("Latitude:  ");Serial.println(latitude_ser);
 //Serial.print("Longitude: ");Serial.println(longitude_ser);
@@ -30,8 +33,9 @@ longitude_ser= a.substring(14,22); longitude = longitude_ser.toFloat();
 //longitude = longitude_ser.toFloat();
 
 Serial.print("RSSI:      ");Serial.println(rssi);
-Serial.print("Latitude:  ");Serial.println(latitude,7);
-Serial.print("Longitude: ");Serial.println(longitude,7);
+Serial.print("Latitude:  ");Serial.println(latitude,6);
+Serial.print("Longitude: ");Serial.println(longitude,6);
+Serial.print("Altitude: ");Serial.println(altitude,2);
 
 
 
